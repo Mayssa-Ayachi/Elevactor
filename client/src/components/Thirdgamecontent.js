35d5d493e2dd1f6cca0efc9df89620144dfb8b94
Mayssa Ayachi <ayachi.mayssa@ensi-uma.tn>
2022-08-13 12:24:38 +0100

    setscore

--- client/src/components/Thirdgamecontent.js
+++ client/src/components/Thirdgamecontent.js
@@ -2,18 +2,38 @@ import React, { useState,useEffect, useRef  } from "react";
 import {Text,Image,View,StyleSheet,TouchableOpacity} from "react-native";
 
 const Thirdgamecontent = (props) => {
+    const [ref1,setref1]=useState("1");
+    const [ref2,setref2]=useState("2");
+    const [ref3,setref3]=useState("3");
+    const [ref4,setref4]=useState("4");
 
+ 
+    const renderImage = () => {
+        const Images = [
+          { image: props.Imagewatermelon,ref:"1" },
+          { image:  props.Imagemother,ref:"2" },
+          { image: props.Imagefather,ref:"3" },
+          { image: props.Imagecastle,ref:"4" },
+        ];
+        const randomImageIndex = Math.floor(Math.random() * Math.floor(4));
+        return Images[randomImageIndex].image;
+    };    
   
-    const press=()=>{
-        props.setscore("4");
+    const press=(ref)=>{
+ 
+         if (ref!=props.id){
+            props.setscore(props.score-1);
+            console.log(props.score);
+        }
+
         props.setImagewatermelon(props.wrongwatermelon);
         props.setImagemother(props.wrongmother);
         props.setImagefather(props.rightfather);
         props.setImagecastle(props.wrongcastle);
-        setTimeout(()=>{ 
-        props.setNext(props.next)},2000)
+        setTimeout(()=>props.setNext(props.next),2000)
     }
 
+ 
     return(
          <View>
  
@@ -37,23 +57,23 @@ const Thirdgamecontent = (props) => {
                 
                 <View style={styles.allchoices}>
                     <View style={styles.choices} >
-                        <TouchableOpacity onPress={()=>{press()}} style={styles.choice}>
+                        <TouchableOpacity  onPress={()=>press(ref1)} style={styles.choice}>
                             <Image source={props.imagewatermelon} />
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={()=>{press()}} style={styles.choice}>
+                        <TouchableOpacity onPress={()=>press(ref2)} style={styles.choice}>
                             <Image source={props.imagemother} />
                         </TouchableOpacity>
                     </View>
                     <View style={styles.choices} >
-                        <TouchableOpacity onPress={press} style={styles.choice}>
+                        <TouchableOpacity onPress={()=>press(ref3)}  style={styles.choice}>
                             <Image source={props.imagefather} />
                         </TouchableOpacity>   
-                        <TouchableOpacity onPress={()=>{press()}} style={styles.choice}>
+                        <TouchableOpacity onPress={()=>press(ref4)}  style={styles.choice}>
                             <Image source={props.imagecastle} />
                         </TouchableOpacity>
                     </View>
                 </View>
-
+ 
                 <View style={styles.lampe}>
                     <Image source={require('../assets/lampenonalumée.png')} />               
                 </View>
